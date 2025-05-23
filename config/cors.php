b34cf7afb2b8ac708aec32c5b6a28cac585fc989
vinstech14 <vinsmark.adlawan0214@gmail.com>
2025-05-23 14:57:13 +0800

    o

---
+++ config/cors.php
@@ -0,0 +1,21 @@
+
+<?php
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'], // You can specify methods like ['GET', 'POST']
+
+    'allowed_origins' => ['http://192.168.169.6:8081'], // Replace '*' with specific domains in production
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'], // Replace '*' with specific headers if needed
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
---
+++ routes/api.php
@@ -0,0 +1,65 @@
+<?php
+
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Hash;
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', function (Request $request) {
+        $request->user()->currentAccessToken()->delete();
+        return response()->noContent();
+    });
+});
+
+Route::post("/register", function () {
+    $data = request()->validate([
+        'name' => 'required',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|confirmed', // Remove Password::defaults() here
+        'device_name' => 'required'
+    ]);
+
+    $user = User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password']),
+    ]);
+
+    event(new Registered($user));
+
+    return response()->json([
+        'token' => $user->createToken($data['device_name'])->plainTextToken
+    ]);
+});
+
+
+    Route::post('/login', function (Request $request) {
+    $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required'
+    ]);
+    $user =User::where('email', $request->email)->first();
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+
+    }
+        return response()->json([
+            'token' => $user->createToken($request->device_name)->plainTextToken
+        ]);
+    });
+
