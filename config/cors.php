ec5d95dd7cdeddb112cca36735fe50c332b4e582
vnsmrk <vinsmark.adlawan0214@gmail.com>
2025-05-26 13:42:52 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -12,10 +12,10 @@ return [
 
     'allowed_headers' => ['*'], // Replace '*' with specific headers if needed
 
-    'exposed_headers' => [],
+    'exposed_headers' => ["*"],
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
